2040dc31798c0951532cc68f468dc0121c947014
Harshal Sharadchandra Patil <harshal@cloudera.com>
2020-01-03 15:24:12 +0000

    dashboard try v5

--- daskdashboard.py
+++ daskdashboard.py
@@ -24,10 +24,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return redirect("http://10.10.165.195:8787", code=302)
+    return redirect("http://127.0.0.1:8787", code=302)
 
 if __name__ == '__main__':
   
     cdsw_port = int(os.environ.get('CDSW_APP_PORT'))
     app.run(host='127.0.0.1', port=cdsw_port)
+
     
+# try3
+
+from flask import Flask,request,redirect,Response
+import requests
+app = Flask(__name__)
+SITE_NAME = "http://localhost"
+@app.route("/")
+def index():
+    return "Flask is running!"
+@app.route("/<path:path>",methods=["GET"])
+def proxy(path):
+    global SITE_NAME
+    if request.method=="GET":
+        resp = requests.get("http://10.10.171.3:46199")
+        #requests.get(f"{SITE_NAME}{path}")
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+
+if __name__ == "__main__":
+    app.run(debug = False,port=8090)
