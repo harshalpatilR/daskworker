a5f290b8f313c23ef7dba6d27db668fd4cb7f21a
Harshal Sharadchandra Patil <harshal@cloudera.com>
2020-01-21 12:06:34 +0000

    dask usecase

--- Untitled-3.py
+++ Untitled-3.py
@@ -5,3 +5,11 @@ if not os.path.isdir("testdir"):
   
 for c in range(2):
   print(c)
+  
+  
+import pandas as pd
+import s3fs
+
+df = pd.read_csv('s3://harshalpatil-s3/loans_accepted_2007_to_2018Q4.csv')
+df.head()
+df.tail()
