13445e7ccd3dfc13ca98b93cde67a637303104f6
Harshal Sharadchandra Patil <harshal@cloudera.com>
2020-01-22 08:15:41 +0000

    dask usecase

--- Untitled-3.py
+++ Untitled-3.py
@@ -13,3 +13,8 @@ import s3fs
 df = pd.read_csv('s3://harshalpatil-s3/loans_accepted_2007_to_2018Q4.csv')
 df.head()
 df.tail()
+
+
+from IPython.display import IFrame
+from IPython.core.display import display
+display(IFrame("http://10.10.9.12:8787", '700px', '450px'))
