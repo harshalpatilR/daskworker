009a43e57a35234dde27e5f5d6b58e66d526352a
Harshal Sharadchandra Patil <harshal@cloudera.com>
2020-01-24 10:20:56 +0000

    wine data gen

--- GenDataDask.py
+++ GenDataDask.py
@@ -2,7 +2,7 @@ import os
 from sklearn.datasets import make_classification
 import pandas as pd
 X,y= make_classification(n_samples=5000000, n_features=15, \
-                            n_informative=12,n_redundant=2, \
+                            n_informative=12,n_redundant=0, \
                             n_classes=2, random_state=123)
 
 cnames = ["V"+str(i) for i in range(1,16)] 
